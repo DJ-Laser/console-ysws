8e9888a1f5d2cf17d704d7d208570ddc4b854777
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2026-01-05 22:48:13 -0500

    handle release notes and make scroll speed depend on bpm

--- game/rust/src/rhythm/note_manager.rs
+++ game/rust/src/rhythm/note_manager.rs
@@ -43,6 +43,11 @@ pub struct NoteManager {
   #[export]
   conductor: OnEditor<Gd<Conductor>>,
 
+  /// How many pixels represent one beat
+  /// Beat 1 will start x * 1 pixels away
+  /// Beat 2 will start x * 2 pixels away
+  /// Higher values will result in faster scrolling because the note must travel further in the same time
+  /// This value is independent of BPM, higher BPM songs will naturally scroll faster
   #[export]
   #[init(val = 400.0)]
   scroll_speed: f64,
@@ -52,6 +57,7 @@ pub struct NoteManager {
   input_latency_ms: f64,
 
   events: NoteEventQueue,
+  held_events: Vec<AssociatedNoteEvent>,
 
   #[init(load = "res://objects/single_note/single_note.tscn")]
   single_note_scene: OnReady<Gd<PackedScene>>,
@@ -98,14 +104,13 @@ impl INode2D for NoteManager {
       let delta = self.get_event_delta(next_event);
 
       if NoteTimingWindow::is_too_late(delta) {
-        match next_event.event_type() {
-          // Dodge notes should get handled befote the miss window
-          NoteEventType::Dodge => godot_warn!("Dodge note not handles before miss timing"),
-          _ => {
-            self.hit_note(NoteTimingWindow::Miss);
-          }
+        // Dodge notes should get handled befote the miss window
+        if matches!(next_event.event_type(), NoteEventType::Dodge) {
+          godot_warn!("Dodge note not handled before miss timing");
         }
 
+        self.hit_next_note(NoteTimingWindow::Miss);
+        // Continue to remove all missed notes this frame
         continue;
       }
 
@@ -117,19 +122,31 @@ impl INode2D for NoteManager {
       match next_event.event_type() {
         NoteEventType::Hit | NoteEventType::Hold => {
           if Input::singleton().is_action_just_pressed(RHYTHM_INPUT_ACTION) {
-            self.hit_note(rating);
+            self.hit_next_note(rating);
           }
         }
         NoteEventType::Release => {
-          if Input::singleton().is_action_just_released(RHYTHM_INPUT_ACTION) {
-            self.hit_note(rating);
-          }
+          // Releases are handled seperately, and should not be in the main queue
+          godot_warn!("Release event in main event queue");
+          self.events.pop();
         }
         NoteEventType::Dodge => todo!("Dodge note handling"),
       }
 
-      // The current event was handled, break
       break;
+    }
+
+    let released_events: Vec<_> = self
+      .held_events
+      .extract_if(.., |_event| {
+        !Input::singleton().is_action_pressed(RHYTHM_INPUT_ACTION)
+      })
+      .collect();
+
+    for event in released_events {
+      let delta = self.get_event_delta(&event);
+      let rating = NoteTimingWindow::from_delta(delta).unwrap_or(NoteTimingWindow::Miss);
+      self.hit_note(event, rating);
     }
   }
 }
@@ -141,20 +158,32 @@ impl NoteManager {
       return;
     };
 
-    self.events.push(AssociatedNoteEvent::new(event, note));
+    match event.event_type() {
+      NoteEventType::Release => {
+        self.held_events.push(AssociatedNoteEvent::new(event, note));
+      }
+      _ => {
+        self.events.push(AssociatedNoteEvent::new(event, note));
+      }
+    }
   }
 
-  /// Hit the next note in the event queue with the provided rating
-  fn hit_note(&mut self, rating: NoteTimingWindow) {
-    let Some(mut event) = self.events.pop() else {
-      godot_warn!("hit_note called with empty event queue");
-      return;
-    };
-
+  /// Hit the specified not event with the provided rating
+  fn hit_note(&mut self, mut event: AssociatedNoteEvent, rating: NoteTimingWindow) {
     //TODO: Check hold notes for realy release, probably with a vec of notes or something
     event.note.dyn_bind_mut().hit(rating);
     self.add_note(event.note);
     godot_print!("Note {:?}", rating);
+  }
+
+  /// Hit the next note in the event queue with the provided rating
+  fn hit_next_note(&mut self, rating: NoteTimingWindow) {
+    let Some(event) = self.events.pop() else {
+      godot_warn!("hit_note called with empty event queue");
+      return;
+    };
+
+    self.hit_note(event, rating);
   }
 
   // Gets the time between now and the note event in seconds
@@ -174,8 +203,9 @@ impl NoteManager {
   /// Get the position in px for note's beat poition
   #[func]
   pub fn get_note_position(&self, position_beats: f64) -> f64 {
-    let time_offset =
-      (self.get_current_beat() - position_beats) * self.conductor.bind().get_beat_duration();
-    -time_offset * self.scroll_speed
+    let beat_offset = self.get_current_beat() - position_beats;
+    // Scaling changes the relative speed of the notes, so divide to keep the scroll speed relative to global units
+    let scroll_speed = self.scroll_speed / (self.base().get_scale().x as f64);
+    -beat_offset * scroll_speed
   }
 }
